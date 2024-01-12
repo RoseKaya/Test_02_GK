77e9602aa7cf65bb8709bda0e04f5ef162202810
RoseKaya <gbuyruk@gmail.com>
2024-01-12 11:52:32 -0500

    Apple01

---
+++ src/Apple.java
@@ -0,0 +1,8 @@
+public class Apple {
+    public static void main(String[] args) {
+        System.out.println("Apple1");
+        System.out.println("Apple2");
+        System.out.println("Apple3");
+    }
+
+}
