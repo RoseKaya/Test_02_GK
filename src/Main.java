37988c2f714ab972b254fc4e6555a345cf1d6f4b
RoseKaya <gbuyruk@gmail.com>
2024-01-12 11:04:02 -0500

    Changes03

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,8 @@ public class Main {
         System.out.println("Hello Java");
 
         System.out.println("Hello test01");
+        System.out.println("Hello test02");
+        System.out.println("Hello test03");
+
     }
 }
