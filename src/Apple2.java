1fe81b512e03b8c72c7a85b69a79fce08d1a0658
RoseKaya <gbuyruk@gmail.com>
2024-01-12 11:55:09 -0500

    Apple2

---
+++ src/Apple2.java
@@ -0,0 +1,7 @@
+public class Apple2 {
+    public static void main(String[] args) {
+        System.out.println("Apple2_01");
+        System.out.println("Apple2_02");
+        System.out.println("Apple2_03");
+    }
+}
